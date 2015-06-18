da81920e5e8aefbce918b950e34c98fac5b898bd
Junegunn Choi <junegunn.c@gmail.com>
2015-06-18 18:41:52 +0900

    Update project.clj

--- Makefile
+++ Makefile
@@ -1,14 +1,22 @@
 profile?=0.98
 
-all: js
-	node_modules/.bin/jsx --extension jsx resources/jsx/ resources/public/js/
-	lein with-profile $(profile) uberjar
-	lein with-profile $(profile) bin
+all: build release
+
+build: js bin
 
 js:
 	npm install
 	node_modules/.bin/bower install
 	node_modules/.bin/gulp
+	node_modules/.bin/jsx --extension jsx resources/jsx/ resources/public/js/
+
+bin:
+	lein with-profile $(profile) bin
+
+release:
+	mkdir -p releases/
+	cp -v target/$(profile)/hbase-region-inspector-$(profile)-* releases/
+	cp -v target/$(profile)+uberjar/hbase-region-inspector-$(profile)-*.jar releases/
 
 repl:
 	lein with-profile $(profile) repl :connect localhost:9999
@@ -18,4 +26,4 @@ watch:
 	node_modules/.bin/jsx --extension jsx --watch resources/jsx/ resources/public/js/ &
 	lein with-profile $(profile) ring server-headless
 
-.PHONY: all js repl watch
+.PHONY: all build bin release js repl watch
--- project.clj
+++ project.clj
@@ -1,4 +1,12 @@
-(defproject hbase-region-inspector "0.1.0-SNAPSHOT"
+(def project-version "0.1.0")
+(def bins
+  (into {}
+        (for [profile [:0.94 :0.98]]
+          [profile
+           (str "hbase-region-inspector-" (name profile) "-" project-version)])))
+(def jars (into {} (for [[k v] bins] [k (str v ".jar")])))
+
+(defproject hbase-region-inspector "0.1.0-SNAPSHOT" ;; Can't use ~ yet
   :description "HBase region dashboard"
   :url "http://example.com/FIXME"
   :license {:name "MIT"}
@@ -22,22 +30,30 @@
          :nrepl {:start? true
                  :port 9999}}
 
+  :jvm-opts ["-Xmx4g"]
   :main ^:skip-aot hbase-region-inspector.core
   :target-path "target/%s"
+
+  :bin {:jvm-opts ~(if-let [jvm-opts (System/getenv "JAVA_OPTS")]
+                     (clojure.string/split jvm-opts #"\s+")
+                     ["-Xmx2g"])}
+  ;; https://github.com/technomancy/leiningen/issues/1718
   :profiles
-  {:0.94 {:bin {:name "hbase-region-inspector-0.94"}
-          :source-paths ["src/hbase-0.94"]
-          ;; lein with-profile 0.94 deps :tree
-          :dependencies [[org.apache.hbase/hbase "0.94.15-cdh4.7.1"
-                          :exclusions
-                          [javax.xml.bind/jaxb-api org.slf4j/slf4j-api org.slf4j/slf4j-log4j12]]
-                         [org.apache.hadoop/hadoop-common "2.0.0-cdh4.7.1"
-                          :exclusions
-                          [javax.xml.bind/jaxb-api org.slf4j/slf4j-api org.slf4j/slf4j-log4j12]]
-                         [org.slf4j/slf4j-api "1.7.12"]
-                         [org.slf4j/slf4j-log4j12 "1.7.12"]]}
-   :0.98 {:bin {:name "hbase-region-inspector-0.98"}
-          :source-paths ["src/hbase-0.98"]
-          :dependencies [[org.apache.hbase/hbase-client "0.98.6-cdh5.3.3"]
-                         [org.apache.hbase/hbase-common "0.98.6-cdh5.3.3"]]}
+  {:0.94 ^:leaky {:bin {:name ~(:0.94 bins)}
+                  :uberjar-name ~(:0.94 jars)
+                  :source-paths ["src/hbase-0.94"]
+                  ;; lein with-profile 0.94 deps :tree
+                  :dependencies [[org.apache.hbase/hbase "0.94.15-cdh4.7.1"
+                                  :exclusions
+                                  [javax.xml.bind/jaxb-api org.slf4j/slf4j-api org.slf4j/slf4j-log4j12]]
+                                 [org.apache.hadoop/hadoop-common "2.0.0-cdh4.7.1"
+                                  :exclusions
+                                  [javax.xml.bind/jaxb-api org.slf4j/slf4j-api org.slf4j/slf4j-log4j12]]
+                                 [org.slf4j/slf4j-api "1.7.12"]
+                                 [org.slf4j/slf4j-log4j12 "1.7.12"]]}
+   :0.98 ^:leaky {:bin {:name ~(:0.98 bins)}
+                  :uberjar-name ~(:0.98 jars)
+                  :source-paths ["src/hbase-0.98"]
+                  :dependencies [[org.apache.hbase/hbase-client "0.98.6-cdh5.3.3"]
+                                 [org.apache.hbase/hbase-common "0.98.6-cdh5.3.3"]]}
    :uberjar {:aot :all}})
