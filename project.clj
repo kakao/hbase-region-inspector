1f6b03d5cc6fbc171a844f95c15866bdb49a3965
Junegunn Choi <junegunn.c@gmail.com>
2015-07-07 18:59:51 +0900

    0.2.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(def project-version "0.1.0")
+(def project-version "0.2.0")
 (def profiles [:0.94 :0.98])
 (def bins (into {} (for [profile profiles]
                      [profile (format "hbase-region-inspector-%s-%s"
                                       (name profile) project-version)])))
 (def jars (into {} (for [[k v] bins] [k (str v ".jar")])))
 
-(defproject hbase-region-inspector "0.1.0-SNAPSHOT" ; Can't use ~ yet
+(defproject hbase-region-inspector "0.2.0" ; Can't use ~ yet
   :description "HBase region dashboard"
   :url "http://example.com/FIXME"
   :license {:name "MIT"}
